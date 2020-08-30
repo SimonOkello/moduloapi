1b7031db2096074bc721e609dcdb42df91849024
SIMON DENNIS <simonokello93@gmail.com>
2020-08-30 09:47:09 +0300

    feature(Pagination):add pagination

--- expenses/views.py
+++ expenses/views.py
@@ -11,7 +11,7 @@ from .permissions import IsOwner
 class ExpenseListAPIView(ListCreateAPIView):
     serializer_class = ExpenseSerializer
     queryset = Expense.objects.all()
-    permissions = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticated,)
 
     def perform_create(self, serializer):
 
@@ -24,12 +24,8 @@ class ExpenseListAPIView(ListCreateAPIView):
 class ExpenseDetailAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = ExpenseSerializer
     queryset = Expense.objects.all()
-    permissions = (permissions.IsAuthenticated, IsOwner,)
+    permission_classes = (permissions.IsAuthenticated, IsOwner,)
     lookup_field = 'id'
-
-    def perform_create(self, serializer):
-
-        return serializer.save(owner=self.request.user)
 
     def get_queryset(self):
         return self.queryset.filter(owner=self.request.user)
